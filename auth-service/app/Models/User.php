5e16f6c627a5cb1634ba606d11ab7a814be7ebd3
Janrey <janreyhablamina.ifliba@gmail.com>
2026-04-01 21:11:46 +0800

    Add ownership and role-based access control for IP update endpoint

--- auth-service/app/Models/User.php
+++ auth-service/app/Models/User.php
@@ -56,6 +56,8 @@ class User extends Authenticatable implements JWTSubject
 
     public function getJWTCustomClaims(): array
     {
-        return [];
+        return [
+            'role' => $this->role,
+        ];
     }
 }
